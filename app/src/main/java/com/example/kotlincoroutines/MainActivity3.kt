036ce45eb423c98200c2b053653d8305c4a6b960
Swapnil Nangare <swapnilnangare143@gmail.com>
2024-08-05 17:37:11 +0530

    Parent and child job hirayche and cancelation of job o

--- app/src/main/java/com/example/kotlincoroutines/MainActivity3.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity3.kt
@@ -1,16 +1,52 @@
 package com.example.kotlincoroutines
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main3)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            execute()
+        }
+    }
+
+    private suspend fun execute() {
+
+        val parentJob = GlobalScope.launch(Dispatchers.Main) {
+            Log.d("TAG", "Parent Started ")
+
+            val childJob = launch(Dispatchers.IO) {
+                Log.d("TAG", "Child Job Started ")
+                delay(5000)
+                Log.d("TAG", "Child Job Ended  ")
+
+
+            }
+            delay(3000)
+            Log.d("TAG", "Child Job Cancelled ")
+            childJob.cancel()
+            Log.d("TAG", "Parent Ended ")
+        }
+        delay(1000)
+        //  parentJob.cancel()
+        parentJob.join()
+        Log.d("TAG", "Parent Completed")
+
+    }
+
+
+    private suspend fun executeLongRunningTask() {
 
     }
 }
